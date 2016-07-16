335f4d7a6d24936a78cf8aa0c5eef02d4bf46857
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2016-07-16 22:53:19 +0200

    common js module loader using systemjs

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
-define(['./player', './game'], function(player, game) {
+var player = require('./player.js');
+var game = require('./game.js');
 
-    console.log('Loading with RequireJs');
-    
-    var document = document;
+console.log('Loading with SystemJs');
 
-    // add click handler to the start game button
-    document.getElementById('startGame').addEventListener('click', function() {
-        player.setName(document.getElementById('playername').value);
-        game.printGame();
-    });
-    
-    // add click handler to the calculate score button
-    document.getElementById('calculate').addEventListener('click', function() {
-        game.calculateScore();
-    });
-    
-    // set the default number of problems
-    document.getElementById('problemCount').value = game.getProblemCount();
-    
+var document = document;
+
+// add click handler to the start game button
+document.getElementById('startGame').addEventListener('click', function() {
+    player.setName(document.getElementById('playername').value);
+    game.printGame();
 });
+
+// add click handler to the calculate score button
+document.getElementById('calculate').addEventListener('click', function() {
+    game.calculateScore();
+});
+
+// set the default number of problems
+document.getElementById('problemCount').value = game.getProblemCount();
